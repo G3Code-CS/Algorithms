3efdfe15fdff82f8b3b4ae69a58e9432ac159941
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-12-09 19:46:12 -0600

    The refined solution.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,27 +4,10 @@ import argparse
 
 
 def find_max_profit(prices):
-    # pt = tuple(prices)
-    # prices.sort()
-    # if (pt.index(prices[-1]) == 0):
-    #     return (prices[-2] - prices[-1])
-    # else:
-    #     hv = [prices[-1] - pt[i] for i in range(pt.index(prices[-1]))]
-    #     hv.sort()
-    #     return hv[-1]
-    # for i, iprice in enumerate(prices[:0:-1]):
-    #     for j, jprice in enumerate(prices[-(i+2)::-1]):
-    #         print(f'{i, iprice} - {j, jprice}')
     k = [iprice-jprice for i, iprice in enumerate(prices[:0:-1]) for j, jprice in enumerate(prices[-(i+2)::-1])]
-    # print(k)
     k.sort()
     return k[-1]
 
-
-# print(find_max_profit([10, 7, 5, 8, 11, 9]))
-# print(find_max_profit([100, 90, 80, 50, 20, 10]))
-# print(find_max_profit([1050, 270, 1540, 3800, 2]))
-# print(find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]))
 
 if __name__ == '__main__':
     # This is just some code to accept inputs from the command line
