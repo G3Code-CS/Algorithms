6ab19102cb2f0ad4a0ac920085a08275ef27368c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-12-09 20:21:23 -0600

    Completed the recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,12 +4,16 @@ import math
 
 
 def recipe_batches(recipe, ingredients):
-    pass 
+    batches = [math.floor(ingredients.get(key)/value) if (ingredients.get(key)) else 0 for key, value in recipe.items()]
+    print(batches)
+    batches.sort()
+    return batches[0]
+
 
 
 if __name__ == '__main__':
     # Change the entries of these dictionaries to test
     # your implementation with different inputs
-    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5, 'nuts': 10}
     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
