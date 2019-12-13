90e9ef031d9db7b4ea10a8513aa44677c23ec66c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-12-13 17:03:05 -0600

    Getting to do making change using the hints provided

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
 
-import sys
+# import sys
+
 
 def making_change(amount, denominations):
-  pass 
+  #  we can initialize a cache as a list (a dictionary
+  #  would work fine as well) of 0s with a length equal to the amount we're
+  #  looking to make change for.
+    cache = [0] * (amount + 1)
+
+  # If the amount is 0 then there is only one way to make that
+    cache[0] = 1
 
 
-if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+# if __name__ == "__main__":
+#     # Test our your implementation from the command line
+#     # with `python making_change.py [amount]` with different amounts
+#     if len(sys.argv) > 1:
+#         denominations = [1, 5, 10, 25, 50]
+#         amount = int(sys.argv[1])
+#         print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+#     else:
+#         print("Usage: making_change.py [amount]")
