388d2d7abff41872a82c96402cce56568e75f43c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-12-13 12:49:19 -0600

    Eating cookies... stuck in a simple issue. Trying to debug

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,15 +2,34 @@
 
 import sys
 
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
-# recursive solution
-def eating_cookies(n, cache=None):
-  pass
+cache = {}
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+
+# The cache parameter is here for if you want to implement
+# a solution that is more efficient than the naive
+# recursive solution
+def eating_cookies(n):
+    cache = {}
+
+    def number_of_ways(n):
+        if (n<0):
+            return 0
+        elif (n == 0):
+            return 1
+        elif (n not in cache):
+            various_ways = number_of_ways(n-1) + number_of_ways(n-2)
+            + number_of_ways(n-3)
+            cache[n] = various_ways
+        return cache[n]
+    return number_of_ways(n)
+
+
+print(eating_cookies(4))
+
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_cookies = int(sys.argv[1])
+#         print("There are {ways} ways for Cookie Monster to eat {n} cookies."
+#              .format(ways=eating_cookies(num_cookies), n=num_cookies))
+#     else:
+#         print('Usage: eating_cookies.py [num_cookies]')
